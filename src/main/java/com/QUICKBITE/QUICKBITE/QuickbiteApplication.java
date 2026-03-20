8e998491e7e567922f05424ee6d8cf2dc2bbc821
Garv Pathak <garvpathak101@gmail.com>
2026-03-20 18:00:01 +0530

    section2-topic1: IoC container and beans

--- src/main/java/com/QUICKBITE/QUICKBITE/QuickbiteApplication.java
+++ src/main/java/com/QUICKBITE/QUICKBITE/QuickbiteApplication.java
@@ -2,12 +2,19 @@ package com.QUICKBITE.QUICKBITE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class QuickbiteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuickbiteApplication.class, args);
+		ApplicationContext context = SpringApplication.run(QuickbiteApplication.class, args);
+		RestaurantService service = context.getBean(RestaurantService.class);
+		RestaurantService servicen=context.getBean(RestaurantService.class);
+		System.out.println(service.welcomemsg());
+		System.out.println(servicen.welcomemsg());
+		System.out.println(service==servicen);
+
 	}
 
 }
---
+++ src/main/java/com/QUICKBITE/QUICKBITE/RestaurantService.java
@@ -0,0 +1,11 @@
+package com.QUICKBITE.QUICKBITE;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RestaurantService {
+    String welcomemsg()
+    {
+        return "Welcome to QuickBite";
+    }
+}
