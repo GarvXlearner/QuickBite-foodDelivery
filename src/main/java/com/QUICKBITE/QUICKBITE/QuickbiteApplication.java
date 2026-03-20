37c46fcb4a3f6397bc5e7100971e23a5af36aecd
Garv Pathak <garvpathak101@gmail.com>
2026-03-20 16:39:10 +0530

    initial project commit

---
+++ src/main/java/com/QUICKBITE/QUICKBITE/QuickbiteApplication.java
@@ -0,0 +1,13 @@
+package com.QUICKBITE.QUICKBITE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickbiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickbiteApplication.class, args);
+	}
+
+}
